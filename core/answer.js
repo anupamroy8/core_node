350357fce9298de06806d914642358368043bb53
anupamroy8 <anupamroy8@gmail.com>
2020-04-30 20:36:37 +0530

    Completed

--- core/answer.js
+++ core/answer.js
@@ -1,5 +1,86 @@
 7. // Write respective headers object below.
+//General header:
+// Request URL: https://altcampus.io/
+// Request Method: GET
+// Status Code: 200 
+// Remote Address: 128.199.85.71:443
+// Referrer Policy: no-referrer-when-downgrade
+
+// Response Header:
+// access-control-allow-credentials: true
+// access-control-allow-origin: *
+// access-control-expose-headers: 
+// cache-control: max-age=0, private, must-revalidate
+// content-encoding: gzip
+// content-type: text/html; charset=utf-8
+// date: Tue, 21 Apr 2020 14:04:59 GMT
+// server: nginx/1.10.3 (Ubuntu)
+// status: 200
+// x-content-type-options: nosniff
+// x-content-type-options: nosniff
+// x-download-options: noopen
+// x-frame-options: SAMEORIGIN
+// x-frame-options: DENY
+// x-permitted-cross-domain-policies: none
+// x-request-id: 2o3tlf87da3vh9uf1000ccr1
+// x-xss-protection: 1; mode=block
+
+//  Request Header:
+// :authority: altcampus.io
+// :method: GET
+// :path: /
+// :scheme: https
+// accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+// accept-encoding: gzip, deflate, br
+// accept-language: en-US,en;q=0.9
+// cookie: _ga=GA1.2.411751058.1582777181; track_uid=58b85316-629b-11ff-6b9a-5e0cf09422fc; _altcampus_key=SFMyNTY.g3QAAAABbQAAAAtfY3NyZl90b2tlbm0AAAAYSHludTlYcFpNTXh1WEd4SW5DWlZkdz09.AJnKVrp5I-C6ebNyVwK-ZTueRmKZf6xz5EyubpFUQ_s
+// sec-fetch-dest: document
+// sec-fetch-mode: navigate
+// sec-fetch-site: none
+// upgrade-insecure-requests: 1
+// user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36
+
+
 
 8. // Write parsed object below of the url ``http://localhost:3000/api/v3?token=8372fcb8y2874b2t478t6t48cbtbc72t4
 
+const url = require("url");
+var myurl = "http://localhost:3000/api/v3?token=8372fcb8y2874b2t478t6t48cbtbc72t4";
+
+console.log(url.parse(myurl, true));
+
+// Url {
+//     protocol: 'http:',
+//     slashes: true,
+//     auth: null,
+//     host: 'localhost:3000',
+//     port: '3000',
+//     hostname: 'localhost',
+//     hash: null,
+//     search: '?token=8372fcb8y2874b2t478t6t48cbtbc72t4',
+//     query: 'token=8372fcb8y2874b2t478t6t48cbtbc72t4',
+//     pathname: '/api/v3',
+//     path: '/api/v3?token=8372fcb8y2874b2t478t6t48cbtbc72t4',
+//     href: 'http://localhost:3000/api/v3?token=8372fcb8y2874b2t478t6t48cbtbc72t4'
+//   }
+  
+
 9. // Write parsed query object
+
+// Url {
+//     protocol: 'http:',
+//     slashes: true,
+//     auth: null,
+//     host: 'localhost:3000',
+//     port: '3000',
+//     hostname: 'localhost',
+//     hash: null,
+//     search: '?token=8372fcb8y2874b2t478t6t48cbtbc72t4',
+//     query: [Object: null prototype] {
+//       token: '8372fcb8y2874b2t478t6t48cbtbc72t4'
+//     },
+//     pathname: '/api/v3',
+//     path: '/api/v3?token=8372fcb8y2874b2t478t6t48cbtbc72t4',
+//     href: 'http://localhost:3000/api/v3?token=8372fcb8y2874b2t478t6t48cbtbc72t4'
+//   }
+  
--- server/basicServer.js
+++ server/basicServer.js
@@ -1,23 +1,92 @@
-var http = require('http');
 
 // 1. Write script to create a basic http server using createServer method, write 'Welcome to NodeJS' as response and
 // listen for request on port 5555.
+// var http = require('http');
+
+// var server = http.createServer(handleRequest);
+
+// function handleRequest(req, res) {
+//   res.end("Welcome to NodeJS");
+// }
+
+// server.listen(5555, ()=> {
+//   console.log("Server started at port 5555");
+// })
+
 
 // 2. Write script to create a server, send in response the request headers 
 // and add listener on port 6666.
 
+// var http = require('http');
+
+// var server = http.createServer(handleRequest);
+
+// function handleRequest(req, res) {
+//   console.log(req.method);
+//   res.end(JSON.stringify(req.headers));
+// }
+
+// server.listen(5555, ()=> {
+//   console.log("Server started at port 5555");
+// })
+
 // 3. create a server and console request methods and url by doing request 
 // from postman or web browsers.
+
+// var http = require('http');
+
+// var server = http.createServer(handleRequest);
+
+// function handleRequest(req, res) {
+//   console.log(req.method);
+//   res.end(JSON.stringify(req.headers));
+// }
+
+// server.listen(5555, ()=> {
+//   console.log("Server started at port 5555");
+// })
 
 // 4. create a server
   // - set response headers as 'text/html' using res.setHeader property.
   // - write some HTML content in response
   // - listen on port 6000
 
+  // var http = require('http');
+
+  // var server = http.createServer(handleRequest);
+  
+  // function handleRequest(req, res) {
+  //   console.log(req.method);
+  //   res.setHeader("content-type", "text/html")
+  //   res.end("<h1>Welcome to NodeJS</h1>");
+  // }
+  
+  // server.listen(6000, ()=> {
+  //   console.log("Server started at port 6000");
+  // })
+
 // 5. create a server
   // - create a seperate file index.html and write some html content
   // - read the html file content and send it in response in createServer method
   // - don't forget to set header before writing to response
+
+  // var http = require("http");
+  // var fs = require("fs");
+
+  // var server = http.createServer(handleRequest);
+  
+  // function handleRequest(req, res) {
+  //   if(req.url === '/'){
+  //     fs.createReadStream("./index.html").pipe(res)
+  //   } else {
+  //     res.end("Page not found")
+  //   }
+  // }
+  
+  // server.listen(3000, ()=> {
+  //   console.log("Server started at port 3000");
+  // })
+
 
 // 6. create a server
   // - create 3 diffenrent file ie. indx.html, about.html, contact.html
@@ -26,27 +95,152 @@ var http = require('http');
   // - "/about" for about.html file
   // - "/contact" for contact.html file
 
+  // var http = require("http");
+  // var url = require("url");
+  // var fs = require("fs");
+
+  // var server = http.createServer(handleRequest);
+
+
+  // function handleRequest(req, res) {
+  //   var parsedUrl = url.parse(req.url, true)
+  //   if(parsedUrl.path === '/'){
+  //     fs.createReadStream("./index.html").pipe(res)
+  //   } else if(parsedUrl.path === '/about') {
+  //     fs.createReadStream("./about.html").pipe(res)
+  //   } else if(parsedUrl.path === '/contact') {
+  //     fs.createReadStream("./contact.html").pipe(res)
+  //   } else {
+  //     res.end("Page not found")
+  //   }
+  // }
+  
+  // server.listen(3000, ()=> {
+  //   console.log("Server started at port 3000");
+  // })
+
 // 7. create an Server(echoServer)
   // handle post request for incoming data from postman using req as eventEmitter
   // send incoming data back in response
+
+  // var http = require("http");
+  // var url = require("url");
+  // var fs = require("fs");
+
+  // var server = http.createServer();
+
+  // server.on("request", (req,res) => {
+  //   var store = "";
+  //   req.on("data", (c)=> {
+  //     store = store +c;
+  //   })
+  //   req.on("end", () => {
+  //     console.log(store);
+  //     res.end(store)
+  //   })
+  // })
+
+  // server.listen(3000, ()=> {
+  //   console.log("Server started at port 3000");
+  // })
+
 
 // 8. create a server
   // handle json data from postman
   // parse the json data
   // send json data back in response
 
+  // var http = require("http");
+  // var url = require("url");
+  // var fs = require("fs");
+
+  // var server = http.createServer();
+
+  // server.on("request", (req,res) => {
+  //   var store = "";
+  //   req.on("data", (c)=> {
+  //     store = store +c;
+  //   })
+  //   req.on("end", () => {
+  //     res.setHeader("content-type", "application/json")
+  //     res.end(store);
+  //   })
+  // })
+
+  // server.listen(3000, ()=> {
+  //   console.log("Server started at port 3000");
+  // })
+
+
 // 9. create a server
   // handle x-www-urlencoded(form data) coming form postman
   // parse form-data using querystring module
   // send data back in response
 
+  // var http = require("http");
+  // var url = require("url");
+  // var fs = require("fs");
+  // var qs = require("querystring");
+
+  // var server = http.createServer();
+
+  // server.on("request", (req,res) => {
+  //   var store = "";
+  //   req.on("data", (c)=> {
+  //     store = store +c;
+  //   })
+  //   req.on("end", () => {
+  //     var parsedData =  qs.parse(store)
+  //     console.log(parsedData);
+  //     res.end(parsedData.name);
+  //   })
+  // })
+
+  // server.listen(3000, ()=> {
+  //   console.log("Server started at port 3000");
+  // })
+
+
 // 10. create a server and add listener on port 7000
   // send get request on 'http://localhost:7000/new?username=altcampus' from postman or browser
   // parse the request url using 'url' core node module
   // extract protocol, path and pathname, query from the request
-  // send path in response
+  // send path in response;
+
+  // var http = require("http");
+  // var url = require("url");
+  // var fs = require("fs");
+
+  // var server = http.createServer(handleRequest);
+  // function handleRequest(req, res) {
+  //   var parsedUrl = url.parse(req.url, true)
+  //   console.log(parsedUrl.protocol);
+  //   console.log(parsedUrl.path);
+  //   console.log(parsedUrl.pathname);
+  //   console.log(parsedUrl.query);
+  //   res.end(parsedUrl.path)
+  // }
+  
+  // server.listen(7000, ()=> {
+  //   console.log("Server started at port 7000");
+  // })
 
 // 11. Write script to create an absolute and relative path of 'theory.md' from the current file.
   // use path module from core node
 
+  var http = require("http");
+  var path = require("path");
 
+  let filePath = path.join(__dirname, ".theory.md")
+  console.log(filePath);
+  
+  let relPath = ("./theory.md")
+  console.log(relPath);
+
+  var server = http.createServer(handleRequest);
+    function handleRequest(req, res) {
+    res.end("Done")
+    }
+  server.listen(7000, ()=> {
+    console.log("Server started at port 7000");
+  })
